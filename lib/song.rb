cd1efecd0cae2c2220c6d26752a0062090160361
Julie  McManus <julieannesays@gmail.com>
2020-02-03 19:26:34 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -36,8 +36,8 @@ class Song
   end
 
   def self.find_or_create_by_name(song_name)
-    find_by_name(song_name) == nil 
-    else 
+    find_by_name(song_name) == nil
+    else
     create_by_name(song_name)
   end
 end
